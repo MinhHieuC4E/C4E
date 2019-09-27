65a633f649c11d67718454704d952fcf7f360fcb
hieu <minhhieu27121997@gmail.com>
2019-09-27 23:23:15 +0700

    ss2

---
+++ ss2_var/var_js.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+console.log("hello"); //ten
+
+let name="hieu";
+console.log(name);
+var age=22;
+console.log(age);
+let a
+console.log(a);
+
+const pi=3.14;
+console.log(pi);
+// let a =5; //khai bao, gan gia tri
+// console.log(a);
